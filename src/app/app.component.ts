1c1a176913f5ef9546965f53aa69f9110b416a1b
beyram-benelghali <beyram.ghali@gmail.com>
2018-03-26 11:37:18 +0200

    Initial COMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,105 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private http: HttpClient , private modalService: NgbModal){
+  }
+
+  title = 'Internsip  TEST';
+  searched = "";
+  users : any = [];
+  friends : any = [];
+  friendsOffriends : any = [];
+  suggestedFriends : any = [];
+  private baseUrl : string ='http://localhost:4200/assets/data.json';
+
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get(this.baseUrl).subscribe(data => {
+      this.users = data ; 
+    });
+  }
+
+  openFriends(content , idUser) {
+    this.searched = "Friends";
+    this.getFriends(idUser);
+    this.modalService.open(content).result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+
+  getFriends(idUser) {
+    this.friends = [];
+    var user = this.users.filter(obj  => obj.id == idUser);
+    user[0].friends.forEach(element => {
+      this.friends.push(this.users.filter(obj  => obj.id == element)[0]);
+    });
+  }
+
+  openFriendsOfFriends(content , idUser) {
+    this.searched = "Friends of Friends";
+    this.getFriendsOfFriends(idUser);
+    this.modalService.open(content).result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+  getFriendsOfFriends(idUser) {
+    this.friendsOffriends = [];
+    this.getFriends(idUser);
+    this.friends.forEach(element => {
+      element.friends.forEach(el => {
+        var fof = this.users.filter(obj  => obj.id == el)[0];
+        if(!this.IsUserExit(this.friends,fof) && !this.IsUserExit(this.friendsOffriends,fof) && fof.id != idUser ) {
+            this.friendsOffriends.push(fof);
+          }
+      });
+    });
+    this.friends = [];
+    this.friends = this.friendsOffriends;
+  }
+
+  IsUserExit(list,user) : boolean {
+    let exist = false ;
+    list.forEach(e => {
+        if(e.id === user.id) {
+          exist =  true ;
+      } else {
+      }
+    });
+    return exist;
+  }
+
+  openSuggestedFriends(content , idUser) {
+    this.searched = "Suggested Friends";
+    this.getSuggestedFriends(idUser);
+    this.modalService.open(content).result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+  getSuggestedFriends(idUser) {
+    this.suggestedFriends = [] ;
+    this.getFriends(idUser);
+    var user = this.users.filter(obj  => obj.id == idUser);
+    if(user[0].friends.length >= 2 ) {
+      this.users.forEach(el => {
+        if(_.intersection(user[0].friends, el.friends ).length >= 2 && el.id != idUser && !this.IsUserExit(this.friends,el)) {
+          this.suggestedFriends.push(el);
+        }
+      });
+    } else {
+      alert("You should at least have 2 friends");
+    }
+    this.friends = [];
+    this.friends = this.suggestedFriends;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
